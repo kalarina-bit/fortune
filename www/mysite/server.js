0c10dd738d0ce9416fb3da164d16828712200edf
kalarina <cuken107@proton.me>
2026-08-25 14:07:25 +0000

    Update www/mysite/server.js

--- www/mysite/server.js
+++ www/mysite/server.js
@@ -12,6 +12,27 @@ app.get("/api/test", (req, res) => {
   });
 });
 
+// Pages
+app.get("/home", (req, res) => {
+  res.sendFile("home.html", { root: "public" });
+});
+
+app.get("/radio", (req, res) => {
+  res.sendFile("radio.html", { root: "public" });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("about.html", { root: "public" });
+});
+
+app.get("/homepage", (req, res) => {
+  res.sendFile("homepage.html", { root: "public" });
+});
+
+app.get("/homepage01", (req, res) => {
+  res.sendFile("homepage01.html", { root: "public" });
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
